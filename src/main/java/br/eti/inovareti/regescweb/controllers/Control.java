fcccad9429229fe3ea338faaa8ef598db525df69
Iraponan Marinho <iraponan.aires@outlook.com>
2021-07-02 22:06:19 -0300

    Exemplo de uso do Thymeleaf com HttpServletRequest (Baixo nivel JavaWeb)

--- src/main/java/br/eti/inovareti/regescweb/controllers/Control.java
+++ src/main/java/br/eti/inovareti/regescweb/controllers/Control.java
@@ -3,11 +3,14 @@ package br.eti.inovareti.regescweb.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class Control {
 
     @GetMapping("/hello")
-    public String hello() {
+    public String hello(HttpServletRequest request) {
+        request.setAttribute("nome", "Teste de Thymeleaf");
         return "htmls/hello"; // Pega os arquivos que estão na pasta de templates dentro de resources
     }
 }
