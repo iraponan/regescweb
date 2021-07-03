5a9d22e8db83a1ee24887843f269ed5dc38c44de
Iraponan Marinho <iraponan.aires@outlook.com>
2021-07-02 22:09:24 -0300

    Exemplo de uso do Thymeleaf com Model do Spring

--- src/main/java/br/eti/inovareti/regescweb/controllers/Control.java
+++ src/main/java/br/eti/inovareti/regescweb/controllers/Control.java
@@ -1,16 +1,15 @@
 package br.eti.inovareti.regescweb.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class Control {
 
     @GetMapping("/hello")
-    public String hello(HttpServletRequest request) {
-        request.setAttribute("nome", "Teste de Thymeleaf");
+    public String hello(Model model) {
+        model.addAttribute("nome", "Teste de Thymeleaf 2");
         return "htmls/hello"; // Pega os arquivos que estão na pasta de templates dentro de resources
     }
 }
