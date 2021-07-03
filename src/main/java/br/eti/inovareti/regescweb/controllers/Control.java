971f8080c15732c1a270469cd8c4a313def8102a
Iraponan Marinho <iraponan.aires@outlook.com>
2021-07-02 22:14:04 -0300

    Exemplo de uso do Thymeleaf com ModelAndView do Spring

--- src/main/java/br/eti/inovareti/regescweb/controllers/Control.java
+++ src/main/java/br/eti/inovareti/regescweb/controllers/Control.java
@@ -1,15 +1,16 @@
 package br.eti.inovareti.regescweb.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class Control {
 
     @GetMapping("/hello")
-    public String hello(Model model) {
-        model.addAttribute("nome", "Teste de Thymeleaf 2");
-        return "htmls/hello"; // Pega os arquivos que estão na pasta de templates dentro de resources
+    public ModelAndView hello() {
+        ModelAndView mv = new ModelAndView("htmls/hello"); // Arquivo HTML a ser redenrizado.
+        mv.addObject("nome", "Teste de Thymeleaf 3");
+        return mv;
     }
 }
